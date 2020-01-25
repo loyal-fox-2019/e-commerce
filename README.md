d966d71293c2894162314744f8815ce5fc97154d
Amil Hasbala <amilhasbala@gmail.com>
2020-01-26 06:13:08 +0700

    change client api config to point to deployed server add deployed server and client link to readme

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # e-commerce
 
 ### Server Base Url
+https://serverwarteg.amilhasbala.com/
 
 ### Documentation
 https://documenter.getpostman.com/view/8028836/SWT8henY
 
 ### Client Deploy
+https://warteghub.amilhasbala.com/
--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosServer = axios.create({
-  baseURL: 'http://localhost:80'
+  baseURL: 'https://serverwarteg.amilhasbala.com'
+  // baseURL: 'http://localhost:80'
 })
 
 export default axiosServer
